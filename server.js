cc87147095ee6c244e19dd4b01abcf44a83eff11
Domuki <anton.linden@dnainternet.net>
2020-10-24 22:24:15 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const e = require('express');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => res.send('API Running'));
+
+app.listen(PORT, () => console.log('Server started on port ${PORT}'));
